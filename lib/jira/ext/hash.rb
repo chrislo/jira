9dd0a041ba9598ecf1563d2009bb260fae22b7ae
Craig R Webster <craig@barkingiguana.com>
2010-01-31 15:11:03 +0000

    Make sure the query string is always in the same order.

--- lib/jira/ext/hash.rb
+++ lib/jira/ext/hash.rb
@@ -2,10 +2,9 @@ require 'cgi'
 
 class Hash
   def to_query
-    query = []
-    self.each_pair do |key,value|
-      query << "#{key}=#{CGI::escape(value.to_s)}"
-    end
-    query.join('&')
+    attributes = keys.dup
+    attributes.sort!{|a,b| a.to_s <=> b.to_s}
+    attributes.map!{|k| "#{k}=#{CGI::escape(self[k].to_s)}"}
+    attributes.join('&')
   end
 end
